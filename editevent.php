4d8bfa1b5f84e97d64b8adf307c5e887749aef69
Benjamin <brpylko@gmail.com>
2015-03-29 22:54:29 -0400

    Match lineups can now be retrieved from The Blue Alliance by inputting a blue alliance event code

--- editevent.php
+++ editevent.php
@@ -4,33 +4,20 @@
 	<link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 <body>
-	<div style="position: fixed; float: right; right: 15%; width: 9%; height:97%; background: #fff"></div>
-	<article class="head">
+	<article>
 		<h1 style="color: #1e208d">Create Event</h1>
 	</article>
-	<article style="padding-top: 40pt; background: #1e208d">
-		<form action="event.php" method="POST" style="text-align: right; padding-top: 3%; width: 87%; background: #fff">
-			<div style="text-align: right; padding-right: 20%">
-				Your Name: <input name="name" required/> Your Team#: <input name="teamno" required/>
+	<article>
+		<form action="event.php" method="POST" style="padding-right: 50%; text-align: right">
+				Your Name: <input name="name" required/>
 				<br/>
-				Event Name: <input name="evtname" required/> Event Code: <input name="evtcode" required/>
+				Your Team#: <input name="teamno" required/>
 				<br/>
-			</div>
-			<?php
-				for($i = 0; $i < 150; $i++)
-				{
-			?>
-			<?php echo(($i+1).". ");?><input type="number" min="1" style="color: red" name="<?php echo(0+6*$i);?>"/>
-			<input type="number" min="1" style="color: red" name="<?php echo(1+6*$i);?>"/>
-			<input type="number" min="1" style="color: red" name="<?php echo(2+6*$i);?>"/>
-			<input type="number" min="1" style="color: blue" name="<?php echo(3+6*$i);?>"/>
-			<input type="number" min="1" style="color: blue" name="<?php echo(4+6*$i);?>"/>
-			<input type="number" min="1" style="color: blue" name="<?php echo(5+6*$i);?>"/>
-			<br/>
-			<?php
-				}
-			?>
-			<input type="submit" value="Save Event" style="position: fixed; top: 13%; right: 16.5%; text-align: center"/>
+				Event Name: <input name="evtname" required/>
+				<br/>
+				<a href="https://docs.google.com/spreadsheet/ccc?key=0ApRO2Yzh2z01dExFZEdieV9WdTJsZ25HSWI3VUxsWGc">The Blue Alliance</a> Event Code: <input name="evtcode" required/>
+				<br/>
+			<input type="submit" value="Save Event"/>
 		</form>
 	</article>
 </body>
--- event.php
+++ event.php
@@ -1,27 +1,49 @@
 <html>
 <head>
 	<title>Processing...</title>
-	<script>
+	<!--<script>
 		window.onload = function()
 		{
 			window.location="index.php";
 		}
-	</script>
+	</script>-->
 </head>
 <body>
 	Processing...
 	<?php
+		#from stackoverflow
+		function curl_get_contents($url)
+		{
+		$ch = curl_init();
+
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_URL, $url);
+
+		$data = curl_exec($ch);
+		curl_close($ch);
+
+		return $data;
+		}
+
 		flush();
 		ob_flush();
 		mkdir("events");
 		mkdir("events/".$_POST["evtcode"]);
 		file_put_contents("events/".$_POST["evtcode"]."/name", $_POST["evtname"], LOCK_EX);
-		$i = 905;
-		for(; $_POST["$i"] == ""; --$i);
-		for($j = 0; $j <= $i/6; ++$j)
+		$teamdata = curl_get_contents("http://www.thebluealliance.com/api/v2/event/2014onwi/matches?X-TBA-App-Id=frc3181:CrowdScouting:git");
+		$teams = [];
+		preg_match_all("~frc([0-9]+)~m", $teamdata, $teams);
+		var_dump($teams[1]);
+		for($i = 1; $i <= count($teams[1])/6; ++$i)
 		{
-			mkdir("events/".$_POST["evtcode"]."/".($j+1));
-			file_put_contents("events/".$_POST["evtcode"]."/".($j+1)."/lineup", $_POST[$j*6]."\n".$_POST[$j*6+1]."\n".$_POST[$j*6+2]."\n".$_POST[$j*6+3]."\n".$_POST[$j*6+4]."\n".$_POST[$j*6+5], LOCK_EX);
+			$matchstring = "";
+			for($j = 0; $j < 6; ++$j)
+			{
+				$matchstring .= $teams[1][$i*6+$j-6]."\n";
+			}
+			mkdir("events/".$_POST["evtcode"]."/$i");
+			file_put_contents("events/".$_POST["evtcode"]."/$i/lineup", $matchstring, LOCK_EX);
 		}
 		file_put_contents("scouters", $_POST["name"]."\n".$_POST["teamno"]."\n", LOCK_EX | FILE_APPEND);
 		file_put_contents("events/".$_POST["evtcode"]."/edited", $_POST["name"]." ".$_POST["teamno"]."\n", LOCK_EX | FILE_APPEND);
